28b9589e1812c574008378a7a75c9bfff8b527ca
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 07:03:50 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:03:50 up 2 days,  3:27,  1 user,  load average: 0.74, 0.48, 0.33 ff8deb4bc15a2a2c2761ee31b62c7c94a75bd45f

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -115,11 +115,11 @@ static int cmd_w(char *args) {
 	strncpy(wp->str,args,strlen(args)+1);
 	bool flag = true;
 	wp->result = expr(args,&flag);
-	//if(!flag){
-	//	printf("Cannot recognise the expression!\n");
-	//	free_wp(wp);
-	//	return 0;
-	//s}
+	if(!flag){
+		printf("Cannot recognise the expression!\n");
+		free_wp(wp);
+		return 0;
+	}
 	//printf("0x%x\n",wp->result);
 	return 0;
 }
