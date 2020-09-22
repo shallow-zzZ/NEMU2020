afdc398c986171a4e55e44f4524d46a418cea765
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 15:41:49 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:41:49 up  8:09,  1 user,  load average: 0.24, 0.18, 0.23 dd1f31b270df2841b304c365f32ef0cb9b094b5d

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -7,7 +7,7 @@ make_helper(concat(cmp_i2rm_,SUFFIX)){
 	int len = concat(decode_rm_,SUFFIX)(eip+1);
 	int8_t src1 = swaddr_read(eip+ len +1,1);
 	print_asm("%x %s",src1, op_src->str);
-	return eip+len+2;
+	return len+2;
 }
 
 #include "cpu/exec/template-end.h"
