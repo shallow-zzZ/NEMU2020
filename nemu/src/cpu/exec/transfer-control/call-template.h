7176dad77df79d27dde76be71f457c5a1a7e0f0a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 09:42:11 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:42:11 up  2:09,  1 user,  load average: 0.40, 0.25, 0.23 37aed072389f479fae964f3592e2956609016c9

--- nemu/src/cpu/exec/transfer-control/call-template.h
+++ nemu/src/cpu/exec/transfer-control/call-template.h
@@ -2,20 +2,19 @@
 
 #define instr call
 
-static void do_execute() {
+make_helper(concat(call_i_,SUFFIX)) {
+	int len = concat(decode_i_,SUFFIX)(eip+1);
 	if(DATA_BYTE == 2){
 		reg_l(R_ESP) -= 2;
-		swaddr_write(reg_l(R_ESP), 2, cpu.eip);
-		cpu.eip = (cpu.eip+ op_src->val)& 0x0000ffff;
-	}else{
-		reg_l(R_ESP)-=4;
-		swaddr_write(reg_l(R_ESP), 4, cpu.eip);
-		cpu.eip = cpu.eip + op_src->val;
+		swaddr_write(reg_l(R_ESP), 2, cpu.eip + len);
+		cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+	}else {
+		reg_l(R_ESP) -= 4;
+                swaddr_write(reg_l(R_ESP), 4, cpu.eip);
+                cpu.eip = cpu.eip + op_src->val;
 	}
-	//print_asm_template1();
+	print_asm(str(isntr) str(SUFFIX) " %x", cpu.eip);
+	return len+1;
 }
-
-make_instr_helper(i)
-make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/transfer-control/call.h
+++ nemu/src/cpu/exec/transfer-control/call.h
@@ -1,8 +1,8 @@
 #ifndef __CALL_H_
 #define __CALL_H_
 
-make_helper(call_i_v)
-make_helper(call_rm_v)
+make_helper(call_i_v);
+//make_helper(call_rm_v);
 
 #endif
 
