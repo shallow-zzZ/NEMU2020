9c159e49d108fa1d24218229ebb2eaacbe9e2e36
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 12:32:14 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:32:14 up  4:59,  1 user,  load average: 0.26, 0.27, 0.17 b6a67c74fd1feb0754d50fc9e6407059cfaa82a

--- nemu/src/cpu/exec/transfer-control/call-template.h
+++ nemu/src/cpu/exec/transfer-control/call-template.h
@@ -17,4 +17,19 @@ make_helper(concat(call_i_,SUFFIX)) {
 	return len+1;
 }
 
+make_helper(concat(call_rm_,SUFFIX)) {
+	int len = concat(decode_rm_,SUFFIX)(eip+1);
+	if(DATA_BYTE == 2){
+                reg_l(R_ESP) -= 2;
+                swaddr_write(reg_l(R_ESP), 2, cpu.eip + len);
+                cpu.eip = op_src->val & 0x0000ffff;
+        }else {
+                reg_l(R_ESP) -= 4;
+                swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
+                cpu.eip = op_src->val;
+        }
+	cpu.eip -= (len+1);
+	return len+1;
+}
+
 #include "cpu/exec/template-end.h"
