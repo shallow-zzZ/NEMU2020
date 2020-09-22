44eaeffe26c7d4b9070a9f0cb830a098992bbf6c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 16:48:37 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:48:37 up  9:15,  1 user,  load average: 0.14, 0.23, 0.18 8b1b2719ef29004afd7ecea79313322b5b447f3

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -6,6 +6,7 @@ make_helper(concat(pop_r_,SUFFIX)){
 	int len = concat(decode_r_,SUFFIX)(eip+1);
 	DATA_TYPE_S num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
 	op_src->reg = num;
+	reg_l(R_ESP) -= DATA_BYTE;
 	print_asm(str(instr) " %s", op_src->str);
 	return len+1;
 }
