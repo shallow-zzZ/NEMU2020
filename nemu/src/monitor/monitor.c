2e14b9b6ed505abbd00e3521e8dbace75c4238a8
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 14:05:17 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:05:17 up 17 min,  1 user,  load average: 0.13, 0.26, 0.24 fc2c27adae88741974ee73659766fdad8ccaf7

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -1,7 +1,7 @@
 #include "nemu.h"
 
 #define ENTRY_START 0x100000
-
+#define EFLAGS_START 0x00000002
 extern uint8_t entry [];
 extern uint32_t entry_len;
 extern char *exec_file;
@@ -86,7 +86,7 @@ void restart() {
 
 	/* Set the initial instruction pointer. */
 	cpu.eip = ENTRY_START;
-
+	cpu.eflags = EFLAGS_START;
 	/* Initialize DRAM. */
 	init_ddr3();
 }
