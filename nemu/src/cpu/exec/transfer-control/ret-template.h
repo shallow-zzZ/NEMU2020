152c6339a95f4c438f490a738a3ef9091762f381
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 11:37:08 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:37:08 up 1 min,  1 user,  load average: 1.91, 0.71, 0.25 9c2a43eda8b11d236c8114d36e0d7f6a4bcd105

--- nemu/src/cpu/exec/transfer-control/ret-template.h
+++ nemu/src/cpu/exec/transfer-control/ret-template.h
@@ -2,7 +2,7 @@
 
 #define instr ret
 
-make_helper(ret){
+make_helper(concat(ret_,SUFFIX)){
 	DATA_TYPE_S num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
 	cpu.eip = num;
 	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
