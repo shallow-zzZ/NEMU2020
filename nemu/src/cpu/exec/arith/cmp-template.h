4a13d01f5cde5cbeb623722c62e6925c84e817a0
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 08:30:38 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:30:38 up 1 day, 58 min,  1 user,  load average: 0.59, 0.36, 0.33 78297a5cd3f07e0f5b2d1f037617b840cd1af89

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -27,11 +27,30 @@ make_helper(concat(cmp_i2rm_,SUFFIX)){
 	return len+2;
 }
 */
-
+#if DATA_BYTE == 2 || DATA_BYTE ==4
 static void do_execute(){
-	print_asm("%s %s",op_src->str, op_dest->str);
+	DATA_TYPE_S src1 = op_dest->val;
+	DATA_TYPE_S src2 = op_src->val;
+	DATA_TYPE_S res = src1 - src2;
+	if(src1>0 && src2<0 && res<0){
+		cpu.OF = 1;
+	}else if(src1<0 && src2>0 && res>0){
+		cpu.OF = 1;
+	}else {
+		cpu.OF = 0;
+	}
+	cpu.SF = (res<0)? 1:0;
+	cpu.ZF = (!res)? 1:0;
+	cpu.CF = 0;
+	int i = 0, cnt = 0;
+	for(i=0;i<8;i++){
+		if(res & (1<<i)) cnt++;
+	}
+	cpu.PF = (cnt%2)? 0:1;
+	print_asm("%s %s %d",op_src->str, op_dest->str, src1);
 }
 
 make_instr_helper(si2rm)
+#endif
 
 #include "cpu/exec/template-end.h"
