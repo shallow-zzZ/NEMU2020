3c1632115a804b0a36235b90ce296aaf3770c37d
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 14:27:11 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:27:11 up 6 min,  1 user,  load average: 0.46, 0.52, 0.30 a903f39c101fa5ddf5cd790187027e62103799ae

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -52,6 +52,7 @@ static void do_execute(){
 }
 
 make_instr_helper(si2rm)
+make_instr_helper(r2rm)
 #endif
 
 /*
--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -13,3 +13,4 @@
 #undef DATA_BYTE
 
 make_helper_v(cmp_si2rm)
+make_helper_v(cmp_r2rm)
