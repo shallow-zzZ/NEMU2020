41ef6a1a5432ba48ced080dc455bb6242ce6e8e6
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 15:45:18 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:45:18 up 15 min,  1 user,  load average: 0.70, 0.57, 0.33 92cf973a692f501769266afd865c598582c2342

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -1,6 +1,6 @@
 #include "cpu/exec/template-start.h"
 
-#define instr sub
+#define instr add
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 
@@ -8,8 +8,8 @@ static void do_execute(){
 	int8_t tmp = op_src->val;
 	DATA_TYPE_S src = (DATA_TYPE_S)tmp;
 	DATA_TYPE_S dest = op_dest->val;
-	if(op_dest->type == OP_TYPE_REG) { REG(op_dest->reg) = dest-src; }
-	else if(op_dest->type == OP_TYPE_MEM) { swaddr_write(op_dest->addr, DATA_BYTE, dest-src); }
+	if(op_dest->type == OP_TYPE_REG) { REG(op_dest->reg) = dest+src; }
+	else if(op_dest->type == OP_TYPE_MEM) { swaddr_write(op_dest->addr, DATA_BYTE, dest+src); }
 	// flags
 	print_asm_template2();
 }
