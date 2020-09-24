c755d0a01d033de161253d4453317d380b6abcd1
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-24 14:21:54 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:21:54 up 1 min,  1 user,  load average: 1.43, 0.52, 0.19 ea22be90d4dc3e5587f1aedc784381709e157de0

--- nemu/src/cpu/exec/data-mov/leave-template.h
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -6,6 +6,7 @@ make_helper(concat(leave_,SUFFIX)) {
 	reg_l(R_ESP) = reg_l(R_EBP);
 	DATA_TYPE_S src = swaddr_read(reg_l(R_ESP), DATA_BYTE);
 	reg_l(R_EBP) = src;
+	reg_l(R_ESP) += DATA_BYTE;
 	print_asm(str(instr));
 	return 1;
 }
