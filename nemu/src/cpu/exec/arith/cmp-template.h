5773e76cdab9027268ea56332c1cbaa9dff9addc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 14:36:52 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:36:52 up  2:14,  1 user,  load average: 0.02, 0.26, 0.32 6216a3aa32b15c4fa1e3bcf2bd36772ade6b0a4

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -2,38 +2,12 @@
 
 #define instr cmp
 
-/*
-make_helper(concat(cmp_i2rm_,SUFFIX)){
-	int len = concat(decode_rm_,SUFFIX)(eip+1);
-	int8_t src1 = swaddr_read(eip + len +1,1);
-	print_asm(str(instr) " " str(SUFFIX) " $0x%x %s",src1, op_src->str);
-	DATA_TYPE_S src2 = op_src->val;
-	DATA_TYPE_S res = src2 - src1;
-	if(src2>0 && src1<0 && res<0){
-		cpu.OF = 1;
-	}else if(src2<0 && src1>0 && res>0){
-		cpu.OF = 1;
-	}else {
-		cpu.OF = 0;
-	}
-	cpu.SF = (res<0)? 1:0;
-	cpu.ZF = (!res)? 1:0;
-	cpu.CF = 0;
-	int i = 0, cnt = 0;
-	for(i=0;i<8;i++){
-		if(res & (1<<i)) cnt++;
-	}
-	cpu.PF = (cnt%2)? 0:1;
-	return len+2;
-}
-*/
 
 #if DATA_BYTE == 2 || DATA_BYTE ==4
 static void do_execute(){
 	DATA_TYPE_S src1 = op_dest->val;
 	DATA_TYPE_S src2 = op_src->val;
 	DATA_TYPE_S res = src1 - src2;
-	DATA_TYPE ures = (DATA_TYPE) res;
 	if(src1>0 && src2<0 && res<0){
 		cpu.OF = 1;
 	}else if(src1<0 && src2>0 && res>0){
@@ -43,7 +17,7 @@ static void do_execute(){
 	}
 	cpu.SF = (res<0)? 1:0;
 	cpu.ZF = (!res)? 1:0;
-	cpu.CF = (op_src->val > ures)? 1:0;
+	cpu.CF = (op_dest->val < op_src->val)? 1:0;
 	int i = 0, cnt = 0;
 	for(i=0;i<8;i++){
 		if(res & (1<<i)) cnt++;
