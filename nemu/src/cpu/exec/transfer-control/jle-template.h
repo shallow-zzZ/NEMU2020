3f89ec8fffe2a1a23b86ee212a8c63af3ade254b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 17:09:19 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:09:19 up  4:47,  1 user,  load average: 0.39, 0.54, 0.53 a5958bd24c12fe424a270c235bcfa8a1e069d9c8

--- nemu/src/cpu/exec/transfer-control/jle-template.h
+++ nemu/src/cpu/exec/transfer-control/jle-template.h
@@ -2,8 +2,9 @@
 
 #define instr jle
 
-make_helper(concat(jle_i_,SUFFIX)){
-	int len = concat(decode_i_,SUFFIX)(eip+1);
+#if DATA_BYTE == 1
+make_helper(concat(jle_si_,SUFFIX)){
+	int len = concat(decode_si_,SUFFIX)(eip+1);
 	if(cpu.ZF || cpu.SF!=cpu.OF){
 		cpu.eip += op_src->val;
 		if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
@@ -11,6 +12,7 @@ make_helper(concat(jle_i_,SUFFIX)){
 	print_asm(str(instr) " %x", cpu.eip + len + 1);
 	return len+1;
 }
+#endif
 
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/transfer-control/jle.h
+++ nemu/src/cpu/exec/transfer-control/jle.h
@@ -1,6 +1,6 @@
 #ifndef __JLE_H_
 #define __JLE_H_
 
-make_helper(jle_i_b);
+make_helper(jle_si_b);
 
 #endif
