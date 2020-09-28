373e9e29fdef2bc65c429ac3e4f7a4a57d179fcc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 13:35:47 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:35:47 up  1:13,  1 user,  load average: 0.36, 0.58, 0.58 493758f441af33975227f99c3a6fe49561bda36

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -33,6 +33,7 @@ static void do_execute(){
 	DATA_TYPE_S src1 = op_dest->val;
 	DATA_TYPE_S src2 = op_src->val;
 	DATA_TYPE_S res = src1 - src2;
+	DATA_TYPE ures = (DATA_TYPE) res;
 	if(src1>0 && src2<0 && res<0){
 		cpu.OF = 1;
 	}else if(src1<0 && src2>0 && res>0){
@@ -42,13 +43,13 @@ static void do_execute(){
 	}
 	cpu.SF = (res<0)? 1:0;
 	cpu.ZF = (!res)? 1:0;
-	cpu.CF = 0;
+	cpu.CF = (op_src->val > ures)? 1:0;
 	int i = 0, cnt = 0;
 	for(i=0;i<8;i++){
 		if(res & (1<<i)) cnt++;
 	}
 	cpu.PF = (cnt%2)? 0:1;
-	print_asm(str(instr) " %s %s",op_src->str, op_dest->str);
+	print_asm(str(instr) " %s %s %x",op_src->str, op_dest->str, op_dest->val);
 }
 
 make_instr_helper(si2rm)
