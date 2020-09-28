ded79361166be6445a23e28f1fbfb02861e27fb4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 14:54:47 +0000

    >  run 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:54:47 up  2:32,  1 user,  load average: 0.50, 0.43, 0.40 cf489a1dd38340b9b7ff5deef0d6f23a14c31727

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -11,7 +11,7 @@ long long ans[] = {0LL,0x1LL, 0x1LL, 0x2LL};//, 0x1LL, 0x2LL, 0x7fffffffffffffff
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-	int i, j;//, ans_idx = 0;
+	/*int i, j;//, ans_idx = 0;
 	int loop = 0;
 	for(i = 0; i < NR_DATA; i ++) {
 		for(j = 0; j < NR_DATA; j ++) {
@@ -19,8 +19,9 @@ int main() {
 			loop ++;
 			//set_bp();
 		}
-	}
+	}*/
 	//nemu_assert(loop == NR_DATA * NR_DATA);
+	nemu_assert(add(test_data[1], test_data[1]) == ans[3]);
 
 	return 0;
 }
