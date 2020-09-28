6fc1b3cc00efa69d6f27a7134b4b0fc566903355
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 17:32:00 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:32:00 up  5:09,  1 user,  load average: 0.35, 0.44, 0.49 76051f367e887f283f4d054a232982c6ebe86320

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -39,6 +39,7 @@
 #include "transfer-control/je.h"
 #include "transfer-control/jne.h"
 #include "transfer-control/jle.h"
+#include "transfer-control/jg.h"
 #include "transfer-control/ret.h"
 #include "transfer-control/jmp.h"
 #include "transfer-control/jbe.h"
---
+++ nemu/src/cpu/exec/transfer-control/jg.h
@@ -0,0 +1,6 @@
+#ifndef __JG_H_
+#define __JG_H_
+
+make_helper(jg_i_b);
+
+#endif
