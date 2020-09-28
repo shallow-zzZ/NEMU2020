ca95720eb20011adf39d7b278c7f58d2fd1255cb
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 14:20:32 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:20:32 up  1:58,  1 user,  load average: 0.03, 0.17, 0.35 ce4cf66230bb623eddf7b89ebee3f5152b3cce6e

--- nemu/src/cpu/exec/transfer-control/jbe-template.h
+++ nemu/src/cpu/exec/transfer-control/jbe-template.h
@@ -4,9 +4,11 @@
 
 make_helper(concat(jbe_i_,SUFFIX)) {
 	int len = concat(decode_i_,SUFFIX)(eip+1);
-	DATA_TYPE_S src = op_src->val;
-	cpu.eip += (int32_t)src;
-	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
+	if(cpu.CF || cpu.ZF) {
+		DATA_TYPE_S src = op_src->val;
+		cpu.eip += (int32_t)src;
+		if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
+	}
 	print_asm(str(instr) "  %x",cpu.eip+len+1);
 	return len+1;
 }
