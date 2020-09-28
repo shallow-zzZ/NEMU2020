8a0855db841c48a6b22dadd41746bcb77453ff61
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 17:20:13 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:20:13 up  4:58,  1 user,  load average: 0.80, 0.57, 0.50 9f46842fcb8c171799a97cd4e2ddc45a305a39a

--- nemu/src/cpu/exec/transfer-control/je-template.h
+++ nemu/src/cpu/exec/transfer-control/je-template.h
@@ -4,11 +4,12 @@
 
 make_helper(concat(je_i_,SUFFIX)){
 	int len = concat(decode_i_,SUFFIX)(eip+1);
-	if(cpu.ZF){
-		cpu.eip += op_src->val;
+	DATA_TYPE_S src = op_src->val;
+	print_asm(str(instr) "  %x",cpu.eip+ (int32_t)src+len+1);
+	if(cpu.ZF) {
+		cpu.eip += (int32_t)src;
 		if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
 	}
-	print_asm(str(instr) " %x", cpu.eip + len + 1);
 	return len+1;
 }
 
--- nemu/src/cpu/exec/transfer-control/jle-template.h
+++ nemu/src/cpu/exec/transfer-control/jle-template.h
@@ -2,17 +2,16 @@
 
 #define instr jle
 
-#if DATA_BYTE == 1
-make_helper(concat(jle_si_,SUFFIX)){
-	int len = concat(decode_si_,SUFFIX)(eip+1);
-	if(cpu.ZF || cpu.SF!=cpu.OF){
-		cpu.eip += op_src->val;
+make_helper(concat(jle_i_,SUFFIX)){
+	int len = concat(decode_i_,SUFFIX)(eip+1);
+	DATA_TYPE_S src = op_src->val;
+	print_asm(str(instr) "  %x",cpu.eip+ (int32_t)src+len+1);
+	if(cpu.ZF || cpu.SF!=cpu.OF) {
+		cpu.eip += (int32_t)src;
 		if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
 	}
-	print_asm(str(instr) " %x", cpu.eip + len + 1);
 	return len+1;
 }
-#endif
 
 
 #include "cpu/exec/template-end.h"
