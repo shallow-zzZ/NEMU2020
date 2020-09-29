a74c48d1e7f6e01a213ae509346ea8e88838e0ee
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 17:10:22 +0000

    >  run 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:10:22 up  7:05,  1 user,  load average: 0.66, 0.55, 0.47 c755658eaf84a63cce9fef298c17e769a134abd9

--- testcase/src/max.c
+++ testcase/src/max.c
@@ -19,7 +19,7 @@ int main() {
 		for(j = 0; j < NR_DATA; j ++) {
 			nemu_assert(max(test_data[i], test_data[j]) == ans[ans_idx ++]);
 			//loop ++;
-			//set_bp();
+			set_bp();
 		}
 	}
 	//nemu_assert(max(test_data[0], test_data[0]) == ans[0]);
