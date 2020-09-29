e975cf726ff5a1a656b02a6c8c752e2ef45ff31a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 16:29:49 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:29:49 up  6:25,  1 user,  load average: 0.41, 0.55, 0.52 955debcdbd82d3d1495aa02b23b32164afa38958

--- nemu/src/cpu/exec/transfer-control/call-template.h
+++ nemu/src/cpu/exec/transfer-control/call-template.h
@@ -4,16 +4,15 @@
 
 make_helper(concat(call_i_,SUFFIX)) {
 	int len = concat(decode_i_,SUFFIX)(eip+1);
+	reg_l(R_ESP) -= DATA_BYTE;
+        swaddr_write(reg_l(R_ESP), 4, cpu.eip + 4);
+	cpu.eip += op_src->val;
 	if(DATA_BYTE == 2){
-		reg_l(R_ESP) -= 2;
-		swaddr_write(reg_l(R_ESP), 2, cpu.eip + len);
-		cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+		cpu.eip = cpu.eip & 0x0000ffff;
 	}else {
-		reg_l(R_ESP) -= 4;
-                swaddr_write(reg_l(R_ESP), 4, cpu.eip + len);
                 cpu.eip = cpu.eip + op_src->val;
 	}
-	print_asm(str(instr) " %x", cpu.eip + len + 1);
+	print_asm(str(instr) " %x", cpu.eip + 5);
 	return len+1;
 }
 
@@ -37,7 +36,7 @@ make_helper(concat(call_rm_,SUFFIX)) {
 static void do_execute() {
 	int d = op_src -> val;
 	reg_l(R_ESP) -= DATA_BYTE;
-	swaddr_write(reg_l(R_ESP),4,cpu.eip+5);
+	swaddr_write(reg_l(R_ESP),4,cpu.eip+4);
 	cpu.eip+=d;
 	print_asm_template1();
 }
@@ -45,10 +44,9 @@ static void do_execute() {
 make_helper(concat(call_rm_,SUFFIX)) {
 	int len = concat(decode_rm_,SUFFIX)(eip+1);	
 	int d = op_src -> val;
-	//printf("%x\n",d);
 	reg_l(R_ESP) -= DATA_BYTE;
-	swaddr_write(reg_l(R_ESP),4,cpu.eip+len+1);
-	cpu.eip=d-len-1;
+	swaddr_write(reg_l(R_ESP),4,cpu.eip+len);
+	cpu.eip = d-len-1;
 	print_asm_template1();
 	return len+1;
 }
