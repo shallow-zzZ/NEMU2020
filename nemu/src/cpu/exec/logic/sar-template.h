7eb7f1335349d29a61f5f0cdde88c24a5751467d
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 17:09:10 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:09:10 up 1 day,  7:04,  1 user,  load average: 0.58, 0.33, 0.22 85663d1847b6b4e4ad06281ea04531d66d804716

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,7 +5,8 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-
+	DATA_TYPE_S tmp = op_dest->val;
+	int len = DATA_BYTE << 3;
 	uint8_t count = src & 0x1f;
 	dest >>= count;
 	OPERAND_W(op_dest, dest);
@@ -13,7 +14,15 @@ static void do_execute () {
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
-
+	cpu.OF = 0;
+	cpu.CF = 0;
+	cpu.ZF = !tmp;
+	cpu.SF = tmp >> (len-1);
+	int i, cnt=0;
+	for(i=0;i<8;i++){
+		if(tmp&(1<<i)) cnt++;
+	}
+	cpu.PF = (cnt%2)? 0:1;
 	print_asm_template2();
 }
 
