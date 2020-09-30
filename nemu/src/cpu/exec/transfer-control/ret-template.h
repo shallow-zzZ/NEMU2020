3db7217ce5f38d1ee9063102f8e97aa62eb780b2
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 13:45:56 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:45:56 up 1 day,  3:41,  1 user,  load average: 0.64, 0.61, 0.52 1b03db46e5d1919362df396f53ca4cdc1c5e0d3

--- nemu/src/cpu/exec/transfer-control/ret-template.h
+++ nemu/src/cpu/exec/transfer-control/ret-template.h
@@ -12,12 +12,18 @@ make_helper(concat(ret_,SUFFIX)){
 }
 
 #if DATA_BYTE == 2
-make_helper(concat(ret_i_,SUFFIX)){
-	//int len = concat(decode_i_,w)(eip+1);
+make_helper(ret_i_w){
+	/*//int len = concat(decode_i_,w)(eip+1);
 	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
 	cpu.eip = num;
 	reg_l(R_ESP) += DATA_BYTE;
 	//cpu.esp += 4;
+	print_asm(str(instr));
+	return 1;*/
+	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
+	cpu.eip = num;
+	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
+	reg_l(R_ESP) += DATA_BYTE;
 	print_asm(str(instr));
 	return 1;
 }
