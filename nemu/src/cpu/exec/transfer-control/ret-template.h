99ea1691b58745baba200f96a8e892a5394f7c3f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 13:58:12 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:58:12 up 1 day,  3:53,  1 user,  load average: 0.16, 0.56, 0.54 13c60c23f6e1732d729f5ff178a6a90af3df28e0

--- nemu/src/cpu/exec/transfer-control/ret-template.h
+++ nemu/src/cpu/exec/transfer-control/ret-template.h
@@ -3,10 +3,10 @@
 #define instr ret
 
 make_helper(concat(ret_,SUFFIX)){
-	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
+	DATA_TYPE num = swaddr_read(reg_l(R_ESP),4);
 	cpu.eip = num;
 	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
-	reg_l(R_ESP) += DATA_BYTE;
+	reg_l(R_ESP) += 4;
 	print_asm(str(instr) str(SUFFIX));
 	return 1;
 }
@@ -14,10 +14,10 @@ make_helper(concat(ret_,SUFFIX)){
 #if DATA_BYTE == 2
 make_helper(concat(ret_i_,SUFFIX)){
 	int len = concat(decode_i_,w)(eip+1);
-	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
-	cpu.eip = (uint32_t)num - len;
+	uint32_t num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
+	cpu.eip = num - len;
 	reg_l(R_ESP) += DATA_BYTE;
-	cpu.esp += op_src->val;
+	reg_l(R_ESP) += op_src->val;
 	print_asm(str(instr));
 	return len + 1;
 }
