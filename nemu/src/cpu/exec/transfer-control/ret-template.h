37ee04d7c11549f26a1dcfb548283a6f0de7fb54
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 13:50:12 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:50:12 up 1 day,  3:45,  1 user,  load average: 0.44, 0.55, 0.52 c653191542236e4bdaac9f6a2c0e65f028d426f0

--- nemu/src/cpu/exec/transfer-control/ret-template.h
+++ nemu/src/cpu/exec/transfer-control/ret-template.h
@@ -11,22 +11,17 @@ make_helper(concat(ret_,SUFFIX)){
 	return 1;
 }
 
-
+#if DATA_BYTE == 2
 make_helper(concat(ret_i_,SUFFIX)){
-	/*//int len = concat(decode_i_,w)(eip+1);
+	int len = concat(decode_i_,w)(eip+1);
 	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
-	cpu.eip = num;
+	cpu.eip = (uint32_t)num - len;
 	reg_l(R_ESP) += DATA_BYTE;
-	//cpu.esp += 4;
+	cpu.esp += op_src->val;
 	print_asm(str(instr));
-	return 1;*/
-	DATA_TYPE num = swaddr_read(reg_l(R_ESP),DATA_BYTE);
-	cpu.eip = num;
-	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
-	reg_l(R_ESP) += DATA_BYTE;
-	print_asm(str(instr));
-	return 1;
+	return len + 1;
 }
+#endif
 
 
 #include "cpu/exec/template-end.h"
