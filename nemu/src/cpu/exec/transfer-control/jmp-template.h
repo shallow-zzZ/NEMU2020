28cd69ceaebc2011f4aa9fd208cf03ec31ceb84c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 01:29:04 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:29:04 up 15:24,  1 user,  load average: 0.95, 0.63, 0.56 4f226122d7f5b82f44da08a8f9c85284b74d2f

--- nemu/src/cpu/exec/transfer-control/jmp-template.h
+++ nemu/src/cpu/exec/transfer-control/jmp-template.h
@@ -11,4 +11,16 @@ make_helper(concat(jmp_i_,SUFFIX)) {
 	return len+1;
 }
 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat(jmp_rm_,SUFFIX)) {
+	int len = concat(decode_rm_,SUFFIX)(eip+1);
+	DATA_TYPE src = op_src->val;
+	cpu.eip = src;
+	if(DATA_BYTE == 2) cpu.eip = cpu.eip & 0x0000ffff;
+	print_asm(str(instr) "  %x", cpu.eip);
+	cpu.eip -= (len+1);
+	return len+1;
+}
+#endif
+
 #include "cpu/exec/template-end.h"
