bda0c73acd9f1460271d3344c568e6e7b42a967f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 08:59:58 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:59:58 up 1 day, 19:16,  1 user,  load average: 0.54, 0.35, 0.21 3cc391492f079a8495e6337210190fc13d328072

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -209,7 +209,7 @@ bool check_parentheses(int p, int q) {
 uint32_t eval(int p, int q){
 	if(p>q){  // bad expression
 		printf("p q %d %d\n", p, q);
-		return 0;
+		return 1;
 	}else if(p==q){
 		int n;
 		if(tokens[p].type == HEX) {
