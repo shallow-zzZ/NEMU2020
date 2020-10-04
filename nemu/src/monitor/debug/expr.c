a44a9b158f3bff98edd46bc7c742ce62ca645af0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 09:11:03 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:11:03 up 1 day, 19:27,  1 user,  load average: 0.92, 0.51, 0.33 7dacdd0c5ec1c207cb476b2b754094bb4984902d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -216,7 +216,6 @@ uint32_t eval(int p, int q){
 		} else if (tokens[p].type == DEC) {
 			sscanf(tokens[p].str,"%d",&n);
 		} else if (tokens[p].type == REG) {
-			printf("%s\n", tokens[p].str);
 			int i;
 			for(i=0;i<8;i++){
 				if(!strcasecmp(tokens[p].str+1,regsl[i])) n = reg_l(i);
