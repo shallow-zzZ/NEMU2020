f2c6ffa8effc494a79cbb20e72bdc93c1df36940
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 08:57:23 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:57:23 up 1 day, 19:14,  1 user,  load average: 0.37, 0.34, 0.20 ec7e4cd32194de44f2ff3fc412866fe0f18d8732

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -237,6 +237,7 @@ uint32_t eval(int p, int q){
 		Assert(op!=-1,"Invalid expression!\n");
 		int val1 = (int)(eval(p,op-1));
 		int val2 = (int)(eval(op+1,q));
+		printf("%d %d\n", val1, val2);
 		switch(tokens[op].type){
 			case(PLUS): return val1+val2;
 			case(MINUS): return val1-val2;
