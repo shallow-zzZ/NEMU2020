8a4a6fb222138e150ea30e5d8bf15592464e34f2
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 08:06:33 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:06:33 up 3 days, 19:28,  1 user,  load average: 0.79, 0.46, 0.19 ae48df04d8050551638c95e355afe9f78f98ab3

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -17,6 +17,7 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 }
 
 lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg){
+	printf("%d\n",cpu.cr0.protect_enable);
 	if(cpu.cr0.protect_enable){
 		assert(addr < (1 << cpu.s_cache[sreg].limit));
 		lnaddr_t lnaddr = cpu.s_cache[sreg].base + addr;
