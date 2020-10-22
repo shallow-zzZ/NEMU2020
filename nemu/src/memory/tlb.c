e3ecb943787ea3954531957a8e0ebd53aa37888f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 11:14:36 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:14:36 up 18:48,  1 user,  load average: 0.52, 0.89, 0.77 c078c162449af6345ec6fd1c2217f03631070b80

--- nemu/src/memory/tlb.c
+++ nemu/src/memory/tlb.c
@@ -25,6 +25,7 @@ PTE read_tlb(lnaddr_t addr, bool *success) {
 	for(;i<64;i++) {
 		/* TLB HIT */
 		if(TLB[i].valid && TLB[i].tag == (addr >> 12)) {
+			printf("tlb hit\n");
 			pte.val = TLB[i].pte_cache.val;
 			return pte;
 		}
@@ -35,6 +36,7 @@ PTE read_tlb(lnaddr_t addr, bool *success) {
 
 void update_tlb(PTE pte) {
 	/* when not hit, update the tlb */
+	printf("---------------\n");
 	srand((unsigned)time(NULL));
 	int vic = rand()%64;
 	TLB[vic].pte_cache.val = pte.val;
