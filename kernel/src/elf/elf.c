bcc7b5017b09394b89fc4df677ae053599fb6aea
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 12:11:38 +0000

    >  compile kernel 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:11:38 up 8 days,  8:35,  1 user,  load average: 0.91, 0.73, 0.64 de8f083bd5cb8d1ca5b2a68e7897c33727ef794

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,7 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+			 ide_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
