76bfee3bf3e032834c4e61b4b3408f8772bd8062
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 12:33:34 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:33:34 up 8 days,  8:57,  1 user,  load average: 0.48, 0.51, 0.54 bc1ba8655bd4947852963ad7243e3d23f4f68b3d

--- nemu/src/cpu/exec/misc/out-template.h
+++ nemu/src/cpu/exec/misc/out-template.h
@@ -4,13 +4,13 @@ uint32_t pio_write(ioaddr_t, size_t, uint32_t);
 
 make_helper(concat(out_a2i_,SUFFIX)) {
 	uint8_t imm = instr_fetch(eip+1, 1);
-	pio_write(imm, DATA_BYTE, (DATA_TYPE)reg_l(R_EAX));
+	pio_write(imm, DATA_BYTE, concat(reg_,SUFFIX)(R_EAX));
 	print_asm("out 0x%x", imm);
 	return 2;
 }
 
 make_helper(concat(out_,SUFFIX)) {
-	pio_write(reg_w(R_DX), DATA_BYTE, (DATA_TYPE)reg_l(R_EAX));
+	pio_write(reg_w(R_DX), DATA_BYTE, concat(reg_,SUFFIX)(R_EAX));
 	print_asm("out");
 	return 1;
 }
