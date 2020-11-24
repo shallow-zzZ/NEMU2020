9bc838b7abc1fca0865b046b91ebffb8dadf63e5
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 14:11:16 +0000

    >  compile NEMU 3018216105 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:11:16 up 8 days, 10:34,  1 user,  load average: 0.78, 0.53, 0.53 fac4be7b22ee0aa9567381f5442fd05bcf9d3f6e

--- nemu/src/cpu/exec/transfer-control/call-template.h
+++ nemu/src/cpu/exec/transfer-control/call-template.h
@@ -3,7 +3,13 @@
 #define instr call
 
 make_helper(concat(call_i_,SUFFIX)) {
-	int len = concat(decode_i_,SUFFIX)(eip+1);
+	int len = concat(decode_i_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + len + 1, R_SS);
+	cpu.eip += (DATA_TYPE_S)op_src->val;
+	print_asm("call 0x%x", cpu.eip + len + 1);
+	return len + 1;
+	/*int len = concat(decode_i_,SUFFIX)(eip+1);
 	reg_l(R_ESP) -= DATA_BYTE;
         swaddr_write(reg_l(R_ESP), 4, cpu.eip + 4, R_SS);
 	cpu.eip += (DATA_TYPE_S)op_src->val;
@@ -11,7 +17,7 @@ make_helper(concat(call_i_,SUFFIX)) {
 		cpu.eip = cpu.eip & 0x0000ffff;
 	}
 	print_asm(str(instr) str(SUFFIX) " %x", cpu.eip + len + 1);
-	return len+1;
+	return len+1;*/
 }
 
 make_helper(concat(call_rm_,SUFFIX)) {
